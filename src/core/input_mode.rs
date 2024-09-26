e36e634fb9c7b497e8add11745869444c896b98b
Diego Machaca <diego.machaca@gmail.com>
2024-09-25 23:42:34 -0500

    test: add unit test to app.rs (#4)

--- src/core/input_mode.rs
+++ src/core/input_mode.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq, Eq)]
 pub enum InputMode {
     Normal,
     Editing,
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,3 +2,5 @@ pub mod app;
 pub mod errors;
 pub mod input_mode;
 pub mod pull_request;
+
+pub mod app_test;
